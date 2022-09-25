2f773b5e89ab4a069d6eeeed838f29477e4483cd
EC2 Default User <ec2-user@ip-172-31-7-78.us-west-1.compute.internal>
2022-09-25 12:47:31 +0000

    [Update]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,31 @@ class UsersController < ApplicationController
     @book=Book.new
     @books=Book.all
     @user=User.find(params[:id])
-
   end
+
+
 
   def index
     @book=Book.new
-    @user=User.all
+    @users=User.all
+
   end
 
   def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+    redirect_to user_path(@user.id),notice:'You have updated user successfully.'
+    else
+    render:edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:profile_image,:introduction)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
 
   has_many:books, dependent: :destroy
+  has_one_attached :profile_image
+
+  validates:name,presence:true
+  validates:introduction,presence:true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   def get_profile_image(width, height)
   unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
